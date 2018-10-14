c4fbf6903c250cb206bfde711e8870d4d70f8299
José vicente de sousa <jose.vicente.de.sousa@gmail.com>
2018-10-14 07:04:48 -0300

    Evironment secure
    Segurança de dados do firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyADpdlxj2z0Vgsgq4Osw3eYJQq5ey6QrKM',
-    authDomain: 'angular-tasks-b2747.firebaseapp.com',
-    databaseURL: 'https://angular-tasks-b2747.firebaseio.com',
-    projectId: 'angular-tasks-b2747',
-    storageBucket: 'angular-tasks-b2747.appspot.com',
-    messagingSenderId: '985997532508'
+    apiKey: 'eee',
+    authDomain: 'eee',
+    databaseURL: 'eee',
+    projectId: 'eee',
+    storageBucket: 'eeee',
+    messagingSenderId: 'eeee'
   }
 };
