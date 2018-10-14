5d87078ded017cce822fe694a4f12587f33e5d22
José vicente de sousa <jose.vicente.de.sousa@gmail.com>
2018-10-14 07:06:27 -0300

    Environment secury 2
    segurança do firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyADpdlxj2z0Vgsgq4Osw3eYJQq5ey6QrKM',
-    authDomain: 'angular-tasks-b2747.firebaseapp.com',
-    databaseURL: 'https://angular-tasks-b2747.firebaseio.com',
-    projectId: 'angular-tasks-b2747',
-    storageBucket: 'angular-tasks-b2747.appspot.com',
-    messagingSenderId: '985997532508'
+    apiKey: 'eee',
+    authDomain: 'eee',
+    databaseURL: 'eee',
+    projectId: 'eee',
+    storageBucket: 'eeee',
+    messagingSenderId: 'eeee'
   }
 };
